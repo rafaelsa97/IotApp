b1ef9db75382d02fb39fe0dd41dcbaacfb94f8a0
Rafael Santos de Almeida <rafaelsa97@ufmg.br>
2019-10-27 23:59:01 -0300

    API Taesa funcionando completamente no servidor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const app  = require("express")();
-const path = require('path');
+const express = require("express");
+const app     = express();
+const path    = require('path');
 
 let viewPath = __dirname + "/FrontEnd/TaesaAPI/"; // Taesa API path on project
 
@@ -8,6 +9,20 @@ RotasSimples: {
     {
         res.sendFile(path.join(viewPath + 'index.html')); // Send index page to browser
     });
+    app.use(express.urlencoded());
+    app.post('/', (req, res) => {
+        switch(req.body.Asset){
+            case "Linhas":
+                res.sendFile(path.join(viewPath + 'lines.html')); // Send index page to browser
+                break;
+            case "Torres":
+                res.sendFile(path.join(viewPath + 'towers.html')); // Send index page to browser
+                break;
+            case "Subestações":
+                res.sendFile(path.join(viewPath + 'substations.html')); // Send index page to browser
+                break;
+        }
+    })
 }
 
 //Runs the server and makes it listen to port 3000
