f998af627cc4d7a682db7bfb867a5f0c217363f8
Rafael Santos de Almeida <rafaelsa97@ufmg.br>
2019-10-30 01:17:30 -0300

    Adicionada referência ao bootstrap no node_modules

--- app.js
+++ app.js
@@ -2,32 +2,41 @@ const express = require("express");
 const app     = express();
 const path    = require('path');
 
-let viewPath = __dirname + "/FrontEnd/TaesaAPI/"; // Taesa API path on project
+let viewPath = __dirname + "/FrontEnd/"; // Taesa API path on project
 
-Rotas: {
-    app.get("/",function(req,res)
+API_Taesa: {
+    app.get("/taesa_api",function(req,res)
     {
-        res.sendFile(path.join(viewPath + 'index.html')); // Send index page to browser
+        res.sendFile(path.join(viewPath + '/TaesaAPI/index.html')); // Send index page to browser
     });
 
     app.use(express.urlencoded()); // Gets the submit parameter from POST request
 
-    app.post('/', (req, res) => {
+    app.post('/taesa_api/map', (req, res) => {
         switch(req.body.Asset){
             case "Linhas":
-                res.sendFile(path.join(viewPath + 'lines.html')); // Send index page to browser
+                res.sendFile(path.join(viewPath + '/TaesaAPI/lines.html')); // Send index page to browser
                 break;
             case "Torres":
-                res.sendFile(path.join(viewPath + 'towers.html')); // Send index page to browser
+                res.sendFile(path.join(viewPath + '/TaesaAPI/towers.html')); // Send index page to browser
                 break;
             case "Subestações":
-                res.sendFile(path.join(viewPath + 'substations.html')); // Send index page to browser
+                res.sendFile(path.join(viewPath + '/TaesaAPI/substations.html')); // Send index page to browser
                 break;
         }
     })
 }
 
+usualRouting: {
+    app.use('/bootstrap', express.static(__dirname + '\\node_modules\\bootstrap\\dist')); // Set the css path
+
+    app.get("/",function(req,res)
+    {
+        res.sendFile(path.join(viewPath + 'index.html')); // Send index page to browser
+    });
+}
+
 //Runs the server and makes it listen to port 3000
 app.listen(3000,function(){
-    console.log("Listening on port 3000!")
+    console.log("Listening on port 3000!");
 });
