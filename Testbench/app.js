84b397c58a5cab89b01cf193768dd174cd6a0880
Rafael Santos de Almeida <rafaelsa97@ufmg.br>
2019-10-31 19:23:37 -0300

    Ignorando arquivos da pasta Testbench

--- Testbench/app.js
+++ Testbench/app.js
@@ -1,63 +1,19 @@
-// const express = require("express");
-// const mysql   = require("mysql");
-// const app     = express();
+const express = require("express");
+const app     = express();
+const path    = require('path');
 
-// //Create connection
-// const db = mysql.createConnection({
-//     host:     'localhost',
-//     user:     'root',
-//     password: '123456'
-//     //database: 'nodemysql'
-// });
+let viewPath = __dirname + "/FrontEnd/"; // Taesa API path on project
 
-// // Connect
-// db.connect( function(err) {
-//     if(err){
-//         console.log("Failed to connect to data base: " + err);
-//         throw err;
-//     }
-//     console.log("Mysql connected!");
-// });
+usualRouting: {
+    app.use('/css', express.static(__dirname + '\\Frontend\\Resources\\css')); // Set the bootstrap path
 
-// // Create Data Base
-// app.get("/createdb", (req,res) =>{
-//     let sql = 'CREATE DATABASE nodemysql';
-//     db.query(sql, function(err,result){
-//         if(err){
-//             throw err;
-//         }
-//         else{
-//             res.send('database created!');
-//             console.log(result);
-//         }
-//     });
-// });
+    app.get("/",function(req,res)
+    {
+        res.sendFile(path.join(viewPath + '/index.html')); // Send index page to browser
+    });
+}
 
-// // Start server
-// app.listen(3306,() => {
-//     console.log("Server listening to port 3306.");
-// });
-
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'pfctaesa'
+//Runs the server and makes it listen to port 3000
+app.listen(3000,function(){
+    console.log("Listening on port 3000!");
 });
- 
-connection.connect((err) =>{
-  if(err){
-    throw err;
-  }
-  else{
-    console.log("Connected to database!!!");
-  }
-});
- 
-connection.query('SELECT * FROM pfc.queimadas', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-connection.end();
