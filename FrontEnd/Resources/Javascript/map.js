46f983a5076c70c77bb8b4f250c5c613da41f7ed
Rafael Santos de Almeida <rafaelsa97@ufmg.br>
2019-11-25 18:41:56 -0300

    Added option to choose the way to plot stuff on the map

--- FrontEnd/Resources/Javascript/map.js
+++ FrontEnd/Resources/Javascript/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([-14.2350044, -51.9252815], 5); // Brazilian coordinates
+var mymap = L.map('mapid').setView([-14.2350044, -51.9252815], 5); // Starts the map pointing to Brazil
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,22 +13,32 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
  * * loadAssetPoints()
  * * Load asset points by AJAX request and print them on map
  *  ! This function is executed during page load
- * @param None
 */
 function loadAssetPoints(){
   fetch("Towers") // Make an API call to get Discharges information
   .then(res => res.json())
-  .then(data => plotMarker(data))
+  .then(data => plotMarker(data,"polyline"))
   .catch(err => console.log(err));
 }
 
 /**
- * * plotMarker(object data)
+ * * plotMarker(object data, string mode)
  * * Plot marker points in the map given a coordinate in the plan
  * @param data: element object to be plotted
+ * @param mode: select the plot mode (polyline or marker)
 */
-function plotMarker(data){
-  data.forEach((element) => {
-    L.marker([element.LATITUDE, element.LONGITUDE]).addTo(mymap);
-  })
+function plotMarker(data,mode){
+  switch (mode){ // Plot a specific geometrical figure according to the desired mode
+    case "marker":
+        data.forEach((element) => {
+          L.marker([element.LATITUDE, element.LONGITUDE]).addTo(mymap);
+        });
+      break;
+    case "polyline":
+      debugger;
+      let coordinatesArray = [];
+      data.forEach(element => coordinatesArray.push([element.LATITUDE,element.LONGITUDE]));
+      L.polyline(coordinatesArray, {color: 'red'}).addTo(mymap);
+      break;
+  }
 }
