043404f61689f6ef654fb539f0283af0529fd2f8
rafaelsa97 <rafaelsa97@ufmg.br>
2020-09-05 16:21:08 -0300

    Disabled automatic training and added .pem to gitignore list

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let viewPath = __dirname + "/FrontEnd/"; // Taesa API path on project
 loadPakcages(app,viewPath);
 loadRoutes(app,viewPath);
 
-modelTraining.runModelTraining(0.025); // Runs model training every N minutes
+// modelTraining.runModelTraining(0.025); // Runs model training every N minutes
 
 //Runs the server and makes it listen to port 3000
 app.listen(environmentalVariables.default.port,function(){
